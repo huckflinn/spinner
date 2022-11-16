3949fd88d22b86dc5df16c22c10ffaa7fe873299
Nick Flinn <nicholas@flinn.net>
2022-11-16 13:50:24 -0500

    Add spinner object

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import *
+
+state = {"turn" : 0}
+
+def spinner():
+    clear()
+    angle = state["turn"] / 10
+    right(angle)
+    forward(100)
+    dot(120, "red")
+    back(100)
+    right(120)
+    forward(100)
+    right(60)
+    forward(100)
+    dot(120, "green")
+    back(100)
+    left(60)
+    back(100)
+    right(120)
+    forward(100)
+    left(60)
+    forward(100)
+    dot(120, "blue")
+    back(100)
+    right(60)
+    back(100)
+    right(120)
+    update()
